a33ae0127efb328bbee2d4d69f31f27990585e99
ZaryabKhan420 <zaryabz043@gmail.com>
2023-11-17 19:49:50 +0500

    Final commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "cards",
+    grabCursor: true,
+    navigation: {
+        nextEl: ".secondary-button-02",
+        prevEl: ".secondary-button-01",
+    },
+});
